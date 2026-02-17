4a31ed58265a56eea922ce8d75fcf4c1e050bbcc
pedro_jde <pedrojuliandeelias@gmail.com>
2026-02-17 19:34:32 -0300

    Primera prueba

---
+++ src/Context/ContactsContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState } from "react";
+import { getContacts } from "../services/contactsService";
+
+
+
+export const ContactsContext = createContext(
+    {
+        contacts: [],
+        favorite_name: ''
+    }
+)
+
+const ContactsContextProvider = ({ children }) => {
+    const contacts = getContacts()
+    const [contactsState, setContactsState] = useState(contacts)
+    const provider_values = {
+        contacts: contactsState,
+        favorite_name: 'pepe'
+    }
+
+
+    return (
+        <ContactsContext.Provider 
+            value={provider_values}
+        >
+            {children}
+        </ContactsContext.Provider>
+    )
+}
+
+export default ContactsContextProvider
